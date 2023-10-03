76d57207085d17a9f35af94e0f9a2d59ef864130
MinhQuan020903 <doquan020903@gmail.com>
2023-10-03 13:33:00 +0700

    test text-to-image api

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -6,27 +6,42 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using Unsplasharp;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Dictionary.ViewModel
 {
     public class MainViewModel : BaseViewModel
 
     {
-        public ICommand ButtonCommand { get; set; }
+        //Data binding for text box and image
+        private string _text;
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                _text = value;
+                OnPropertyChanged(nameof(Text));
+            }
+        }
         private string _image;
         public string Image
         {
-            get { return _image; }
+            get => _image;
             set
             {
                 _image = value;
-                OnPropertyChanged();
+                OnPropertyChanged(nameof(Image));
             }
         }
+        public ICommand ButtonCommand { get; set; }
 
         public MainViewModel()
         {
+            //Init button command
             ButtonCommand = new RelayCommand<object>(ButtonCommandCanExecute, ButtonCommandExecute);
+
         }
 
         private bool ButtonCommandCanExecute(object obj)
@@ -34,14 +49,27 @@ namespace Dictionary.ViewModel
             return true;
         }
 
-        private void ButtonCommandExecute(object obj)
+        private async void ButtonCommandExecute(object obj)
         {
-            if (obj != null)
-            {
-                string textFromTextBox = obj.ToString();
-                MessageBox.Show(textFromTextBox);
+                //Connect to Unsplash API with API credential
+                var client = new UnsplasharpClient("F7N8sz1bB94PVtZhq4MQTp4YvwKOh-UNMk2i6M_joS0");
+                try 
+                {
+                        //Search images based from text
+                        var photosFound = await client.SearchPhotos(Text,1, 1);
+                        //Get first image
+                        string url = photosFound[0].Urls.Small;
+                        //Assign imageUrl to Image component
+                        Image = url;
+                        
+                } catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                } 
                 
-            }
+
+            
         }
+
     }
 }
