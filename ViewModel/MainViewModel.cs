03034037bccd0fb7b105b59cc208009ad8da4a43
MinhQuan020903 <doquan020903@gmail.com>
2023-10-03 08:44:50 +0700

    Change directory

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,17 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Dictionary.ViewModel
 {
-    class MainViewModel : BaseViewModel
+    public class MainViewModel : BaseViewModel
+
     {
+        public ICommand ButtonCommand { get; set; }
+        private string _image;
+        public string Image
+        {
+            get { return _image; }
+            set
+            {
+                _image = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainViewModel()
         {
-            MessageBox.Show("Hello World");
+            ButtonCommand = new RelayCommand<object>(ButtonCommandCanExecute, ButtonCommandExecute);
+        }
+
+        private bool ButtonCommandCanExecute(object obj)
+        {
+            return true;
+        }
+
+        private void ButtonCommandExecute(object obj)
+        {
+            if (obj != null)
+            {
+                string textFromTextBox = obj.ToString();
+                MessageBox.Show(textFromTextBox);
+                
+            }
         }
     }
 }
