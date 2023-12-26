347ad3b561afecd2c108d60bddeea4b00e4a1d1a
AkiraNoob <narutoducphuong76@gmail.com>
2023-12-26 09:58:37 +0700

    NetSparkle Updater

--- IndexWindow.xaml.cs
+++ IndexWindow.xaml.cs
@@ -20,21 +20,6 @@ namespace Dictionary
         {
             InitializeComponent();
             IndexFrame.Navigate(new Uri("/View/MainPageView.xaml", UriKind.Relative));
-
-            // get public and secret key in README, just in development, hard code in release
-            var publicKey = "uyhHEMP+TENQzvw/uRbLfdxnvZoGLLRvhqmEeCvlnRc=";
-            var _sparkle = new SparkleUpdater(
-    "https://github.com/MinhQuan020903/Dictionary/tree/release/Update/appcast.xml", // link to your app cast file
-    new Ed25519Checker(SecurityMode.Strict, publicKey) // your base 64 public key -- generate this with the NetSparkleUpdater.Tools.AppCastGenerator .NET CLI tool on any OS
-)
-            {
-                UIFactory = new NetSparkleUpdater.UI.WPF.UIFactory(), // or null or choose some other UI factory or build your own!
-                RelaunchAfterUpdate = false, // default is false; set to true if you want your app to restart after updating (keep as false if your installer will start your app for you)
-                CustomInstallerArguments = "", // set if you want your installer to get some command-line args
-                ShowsUIOnMainThread = true, // required on Avalonia, preferred on WPF/WinForms
-            };
-            _sparkle.StartLoop(true); // `true` to run an initial check online -- only call StartLoop once for a given SparkleUpdater instance!
-            _sparkle.CheckForUpdatesQuietly();
         }
     }
 }
--- ViewModel/UserControlViewModel/ControlBarViewModel.cs
+++ ViewModel/UserControlViewModel/ControlBarViewModel.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using NetSparkleUpdater;
+using NetSparkleUpdater.Enums;
+using NetSparkleUpdater.SignatureVerifiers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,16 +15,34 @@ namespace Dictionary.ViewModel.UserControlViewModel
 {
     public class ControlBarViewModel
     {
+
+        private SparkleUpdater _sparkle;
+
         #region commands
 
         public ICommand CloseWindowCommand { get; set; }
         public ICommand MinimizeWindowCommand { get; set; }
         public ICommand MaximizeWindowCommand { get; set; }
         public ICommand MouseMoveWindowCommand { get; set; }
+        public ICommand GetUpdateCommand { get; set; }
         #endregion
 
         public ControlBarViewModel()
         {
+            string publicKey = "uyhHEMP+TENQzvw/uRbLfdxnvZoGLLRvhqmEeCvlnRc=";
+            // get public and secret key in README, just in development, hard code in release
+            _sparkle = new SparkleUpdater(
+    "https://raw.githubusercontent.com/MinhQuan020903/Dictionary/release/Update/appcast.xml", // link to your app cast file
+        new Ed25519Checker(SecurityMode.Unsafe, publicKey) // your base 64 public key -- generate this with the NetSparkleUpdater.Tools.AppCastGenerator .NET CLI tool on any OS
+)
+            {
+                UIFactory = new NetSparkleUpdater.UI.WPF.UIFactory(), // or null or choose some other UI factory or build your own!
+                RelaunchAfterUpdate = true, // default is false; set to true if you want your app to restart after updating (keep as false if your installer will start your app for you)
+                CustomInstallerArguments = "", // set if you want your installer to get some command-line args
+                ShowsUIOnMainThread = true, // required on Avalonia, preferred on WPF/WinForms
+            };
+            _sparkle.StartLoop(true, true); // `true` to run an initial check online -- only call StartLoop once for a given SparkleUpdater instance!
+
             CloseWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) => { GetParentWindow(p).Close(); });
             MinimizeWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) =>
             {
@@ -36,6 +58,12 @@ namespace Dictionary.ViewModel.UserControlViewModel
                     }
                 }
             });
+
+            GetUpdateCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, async (p) =>
+            {
+               await _sparkle.CheckForUpdatesAtUserRequest();
+            });
+
             MaximizeWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) =>
             {
                 if (GetParentWindow(p) != null)
