17eb35569cdacbd11063245fc03d964db72ec08c
AkiraNoob <narutoducphuong76@gmail.com>
2024-01-01 16:08:26 +0700

    change tooltip and adding version

--- IndexWindow.xaml.cs
+++ IndexWindow.xaml.cs
@@ -20,6 +20,12 @@ namespace Dictionary
         {
             InitializeComponent();
             IndexFrame.Navigate(new Uri("/View/MainPageView.xaml", UriKind.Relative));
+
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+            string? version = fvi.FileVersion;
+
+            currentVersion.Text = version;
         }
     }
 }
